e55f91cb3e5d6badd73acd0fb708e3fedeb5d5fb
phoenixrever <phoenixrever@163.com>
2021-06-26 11:11:59 +0800

    v8.0

--- lib/main.dart
+++ lib/main.dart
@@ -9,65 +9,95 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
         home: Scaffold(
       appBar: AppBar(title: Text("按钮demo")),
-      body: TextFieldDemo(),
+      body: FormDemo(),
       bottomNavigationBar: BottomAppBar(child: Text("bottom")),
     ));
   }
 }
 
-class TextFieldDemo extends StatefulWidget {
+class FormDemo extends StatefulWidget {
   @override
   State<StatefulWidget> createState() {
-    return TextFieldStateDemo();
+    return FormDemoStateDemo();
   }
 }
 
-class TextFieldStateDemo extends State<TextFieldDemo> {
- final textC= TextEditingController();
+class FormDemoStateDemo extends State<FormDemo> {
+  String username = '';
+  String password = '';
 
-  @override
-  void initState() {
-    super.initState();
-    textC.text="我是初始值";
-    textC.addListener(() {
-      print("监听到值的改变====>${textC.text}");  ///监听变化的值
-    });
-  }
+  GlobalKey<FormState> globalFormKey = GlobalKey<FormState>();
+
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: const EdgeInsets.all(8.0),
-      child: Center(
-        child: TextField(
-          decoration: InputDecoration(
-            //装饰品; 装饰图案; 装饰风格;
-            icon: Icon(Icons.people),
-            labelText: "用户名",
-            hintText: "请输入用户名",
-            enabledBorder: OutlineInputBorder(
-              borderSide: BorderSide(
-                color: Colors.red.withOpacity(0.9),  // 边框颜色
-                  width: 2
+      padding: const EdgeInsets.all(20.0),
+      child: Form(
+          key: globalFormKey,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              TextFormField(
+                autovalidateMode: AutovalidateMode.always,
+                decoration: InputDecoration(
+                    icon: Icon(Icons.people), labelText: "username"),
+                // onChanged: (value){
+                //   username=value;
+                // },
+                onSaved: (value) {
+                  username = value!;
+                  print("onsaved-------$value");
+                },
+                validator: (value) {
+                  if (value == null || value.length == 0) return "用户名不能为空";
+                  if (!(value.length >= 6 && value.length <= 20))
+                    return "用户名在6到20位之间";
+                },
               ),
-            ),
-            focusedBorder:OutlineInputBorder(
-              borderSide: BorderSide(
-                color: Colors.blue.withOpacity(0.9),
-                width: 2
+              TextFormField(
+                autovalidateMode: AutovalidateMode.always,
+                obscureText: true,
+                //...作为密码
+                decoration: InputDecoration(
+                    icon: Icon(Icons.lock), labelText: "password"),
+                // onChanged: (value){
+                //   password=value;
+                // },
+                onSaved: (value) {
+                  password = value!;
+                  print("onsaved----$value");
+                },
+                validator: (value) {
+                  if (value == null || value.length == 0) return "密码不能为空";
+                  if (!(value.length >= 6 && value.length <= 20))
+                    return "密码6到20位之间";
+                },
               ),
-            ),
-            filled: true,
-            fillColor: Colors.white10
-          ),
-          onChanged: (value) {
-            print(value);
-          },
-          onSubmitted: (value){
-            print(value);
-          },
-          controller: textC   ///监听值的改变
-        ),
-      ),
+              SizedBox(
+                height: 30,
+              ),
+              Container(
+                width: double.infinity,
+                height: 40,
+                child: ElevatedButton(
+                    onPressed: () {
+                      globalFormKey.currentState!.save();
+                      /// autovalidateMode: AutovalidateMode.always, 开启自动验证 不需要这个
+                      /// 所以 onChange赋值还是能用的
+                      // globalFormKey.currentState!.validate();
+                      print("username:$username<=========>password:$password");
+                    },
+                    child: Text(
+                      "login",
+                      style: TextStyle(fontSize: 20),
+                    ),
+                    style: ElevatedButton.styleFrom(
+                      primary: Colors.purpleAccent,
+                      onPrimary: Colors.white,
+                    )),
+              ),
+            ],
+          )),
     );
   }
 }
